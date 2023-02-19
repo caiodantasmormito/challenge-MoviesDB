d533683a78fe95a680c4a4f73b00c5df2adcfe86
Caio Dantas <caiodantas26@hotmail.com>
2023-02-19 20:11:35 -0300

    add: details page

---
+++ lib/pages/details_page.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import '../models/movies_models.dart';
+import '../utils/api_utils.dart';
+
+class DetailsPage extends StatelessWidget {
+  final Results results;
+  const DetailsPage({
+    Key? key,
+    required this.results,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+        results.title,
+      )),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .50,
+                width: MediaQuery.of(context).size.width,
+                child: Image.network(
+                  API.requestImg(results.posterPath),
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return const CircularProgressIndicator.adaptive();
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: [
+                  const Text('Favorite '),
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Icon(Icons.favorite, size: 16,),
+                  ),
+                  const SizedBox(
+                    width: 180,
+                  ),
+                  Text('Likes: ${results.voteCount.toString()}'),
+                ],
+              ),
+              const SizedBox(height: 16,),
+              Text(
+                results.overview,
+                style: const TextStyle(fontSize: 14),
+              ),
+              const SizedBox(height: 16,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Language: ${results.originalLanguage.toUpperCase()}'),
+                  Text('Relase data: ${results.releaseDate}')
+                ],
+              ),
+              const Divider()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/list_movies_card.dart
+++ lib/widgets/list_movies_card.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:movies_db/models/movies_models.dart';
+import 'package:movies_db/pages/details_page.dart';
 import 'package:movies_db/utils/api_utils.dart';
 
 class ListMoviesCard extends StatelessWidget {
@@ -13,44 +14,61 @@ class ListMoviesCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 180,
-      decoration: const BoxDecoration(
-        color: Colors.black54,
-        borderRadius: BorderRadius.all(Radius.circular(12))
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          ClipRRect(borderRadius: const BorderRadius.only(topLeft: Radius.circular(12), bottomLeft: Radius.circular(12),),
-            child: Image.network(
-              API.requestImg(results.posterPath),
-              
+    return Material(
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => DetailsPage(results: results),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Text(
-                    results.title,
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                    softWrap: true,
-                    overflow: TextOverflow.visible,
-                  ),
-                  
-                  const Spacer(),
-                  Text('Popularidade ${results.popularity}'),
-                  Text('Votos ${results.voteAverage}')
-                ],
+          );
+        },
+        child: Container(
+          height: 180,
+          decoration: const BoxDecoration(
+              color: Colors.black54,
+              borderRadius: BorderRadius.all(Radius.circular(12))),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  bottomLeft: Radius.circular(12),
+                ),
+                child: Image.network(
+                  API.requestImg(results.posterPath),
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return const CircularProgressIndicator.adaptive();
+                  },
+                ),
               ),
-            ),
-          )
-        ],
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        results.title,
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                        softWrap: true,
+                        overflow: TextOverflow.visible,
+                      ),
+                      const Spacer(),
+                      Text('Popularidade ${results.popularity}'),
+                      Text('Votos ${results.voteAverage}')
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
