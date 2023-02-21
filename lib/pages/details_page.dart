8dc61521cc1d01c6f52f4d06ec9410dd17c405e5
Mariana Cano <caiodantas26@hotmail.com>
2023-02-21 19:49:36 -0300

    add: video player

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
 import 'package:flutter/material.dart';
 
 import '../models/movies_models.dart';
 import '../utils/api_utils.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   final Results results;
   const DetailsPage({
     Key? key,
@@ -12,11 +14,29 @@ class DetailsPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  late YoutubePlayerController _playerController;
+
+  @override
+  void initState() {
+    super.initState();
+    _playerController = YoutubePlayerController(
+      initialVideoId: 'XFUy4YCgppk',
+      flags: const YoutubePlayerFlags(
+        autoPlay: false,
+        isLive: false,
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           title: Text(
-        results.title,
+        widget.results.title,
       )),
       body: SingleChildScrollView(
         child: Padding(
@@ -28,7 +48,7 @@ class DetailsPage extends StatelessWidget {
                 height: MediaQuery.of(context).size.height * .50,
                 width: MediaQuery.of(context).size.width,
                 child: Image.network(
-                  API.requestImg(results.posterPath),
+                  API.requestImg(widget.results.posterPath),
                   loadingBuilder: (context, child, loadingProgress) {
                     if (loadingProgress == null) return child;
                     return const CircularProgressIndicator.adaptive();
@@ -43,26 +63,41 @@ class DetailsPage extends StatelessWidget {
                   const Text('Favorite '),
                   GestureDetector(
                     onTap: () {},
-                    child: const Icon(Icons.favorite, size: 16,),
+                    child: const Icon(
+                      Icons.favorite,
+                      size: 16,
+                    ),
                   ),
-                  
-                  
                 ],
               ),
-              const SizedBox(height: 16,),
+              const SizedBox(
+                height: 16,
+              ),
               Text(
-                results.overview,
+                widget.results.overview,
                 style: const TextStyle(fontSize: 14),
               ),
-              const SizedBox(height: 16,),
+              const SizedBox(
+                height: 16,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('Language: ${results.originalLanguage.toUpperCase()}'),
-                  Text('Relase data: ${results.releaseDate}')
+                  Text(
+                      'Language: ${widget.results.originalLanguage.toUpperCase()}'),
+                  Text('Relase data: ${widget.results.releaseDate}')
                 ],
               ),
-              const Divider()
+              const Divider(),
+              YoutubePlayerBuilder(
+                  player: YoutubePlayer(
+                    controller: _playerController,
+                    showVideoProgressIndicator: true,
+                  ),
+                  builder: (_, player) => player),
+              const SizedBox(
+                height: 16,
+              )
             ],
           ),
         ),
